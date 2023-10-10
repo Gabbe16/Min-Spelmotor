8a0ed5a0d436ead115cbafc6dfc3d9f12be69218
Gabriel Nilsson Högberg <gabrielnilsson.hogb@elev.ga.ntig.se>
2023-10-10 09:30:52 +0200

    ✨ Lade till InputHandler

--- src/Game.js
+++ src/Game.js
@@ -1,8 +1,10 @@
 import Player from "./Player.js"
+import InputHandler from "./InputHandler.js"
 export default class Game {
   constructor(width, height) {
     this.width = width
     this.height = height
+    this.input = new InputHandler(this)
     this.keys = []
     this.enemies = []
     this.gameOver = false
--- src/Player.js
+++ src/Player.js
@@ -4,13 +4,25 @@ export default class Player {
         this.width = 32
         this.height = 64
         this.x = 50
-        this.y = 100
+        this.y = 350
 
         this.speedX = 1
         this.speedY = 0
+        this.maxSpeed = 4
+        this.jumpHeight = -5
     }
 
     update(deltaTime) {
+        if (this.game.keys.includes('ArrowLeft')) {
+            this.speedX = -this.maxSpeed
+        } else if (this.game.keys.includes('ArrowRight')) {
+            this.speedX = this.maxSpeed
+        } else if (this.game.keys.includes('ArrowUp')) {
+            // kod för att hoppa här
+        } else {
+            this.speedX = 0
+        }
+        
         this.x += this.speedX
     }
 
