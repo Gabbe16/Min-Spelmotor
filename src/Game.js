e97ef0192d8cbe32c8fb6a069819872c70efe46a
Gabriel Nilsson Högberg <gabrielnilsson.hogb@elev.ga.ntig.se>
2023-10-17 10:01:55 +0200

    ✨ Pumpkins (enemies)

--- src/Game.js
+++ src/Game.js
@@ -1,22 +1,25 @@
-import Player from "./Player.js"
+import Slime from "./Slime.js"
 import InputHandler from "./InputHandler.js"
-import UserInterface from "./UserInterface.js"
 import Platform from "./Platform.js"
+import Player from "./Player.js"
+import UserInterface from "./UserInterface.js"
 
 export default class Game {
   constructor(width, height) {
     this.width = width
     this.height = height
     this.input = new InputHandler(this)
-    this.UI = new UserInterface(this)
+    this.ui = new UserInterface(this)
     this.keys = []
-    this.enemies = []
     this.gameOver = false
-    this.debug = false
-    this.gravity = 1
-    this.gameTime = 0
     this.score = 0
-    this.ground = this.height - 100
+    this.gravity = 1
+    this.debug = false
+    this.gameTime = 0
+
+    this.enemies = []
+    this.enemyTimer = 0
+    this.enemyIntervall = 1000
 
     this.player = new Player(this)
 
@@ -32,46 +35,25 @@ export default class Game {
     }
     this.player.update(deltaTime)
 
-    this.platforms.forEach((enemy) => {
-      if (this.checkPlatformCollision(this.player, Platform)) {
-        this.player.speedY = 0
-        this.player.y = this.platform.y - this.player.height
-        this.player.grounded = true
-      }
-      this.enemies.forEach(() => {
-        if (this.checkPlatformCollision(enemy, platform)) {
-          enemy.speedY = 0
-          enemy.y = platform.y - enemy.height
-        }
-      })
-    })
-  }
-
-  checkPlatformCollision(object, platform) {
-    if (
-      object.y + object.height >= platform.y &&
-      object.y < platform.y &&
-      object.x + object.width >= platform.x &&
-      object.x <= platform.x + platform.width
-    ) {
-      if (object.grounded && object.y + object.height > platform.y) {
-        object.speedY = 0
-        object.y = platform.y - object.height
-        object.grounded = true
-      }
-      return true
+    if (this.enemyTimer > this.enemyIntervall && !this.gameOver) {
+      this.addEnemy()
+      this.enemyTimer = 0
     } else {
-      if (object.grounded && object.y + object.height < platform.y) {
-        object.grounded = false
-      }
-      return false
+      this.enemyTimer += deltaTime
     }
-  }
 
+    this.enemies.forEach((enemy) => enemy.update(deltaTime))
+    this.enemies = this.enemies.filter((enemy) => !enemy.markedForDeletion)
+  }
 
   draw(context) {
-    this.UI.draw(context)
+    this.ui.draw(context)
     this.player.draw(context)
     this.platforms.forEach((platform) => platform.draw(context))
+    this.enemies.forEach((enemy) => enemy.draw(context))
+  }
+
+  addEnemy() {
+    this.enemies.push(new Slime(this, 340, 168))
   }
 }
